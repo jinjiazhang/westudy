976169c9166f4d9130f1c09db1519f0d10aed612
jinjiazhang <jinjiazhang@tencent.com>
2025-05-15 23:42:52 +0800

    修改主题背景色

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,13 @@ class WeStudyApp extends StatelessWidget {
       title: 'WeStudy',
       theme: ThemeData(
         primarySwatch: Colors.blue, // 主题色可根据需要自定义
+        scaffoldBackgroundColor: Colors.white,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+        ),
+        bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+          backgroundColor: Colors.white,
+        ),
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: HomeScreen(), // 主页
